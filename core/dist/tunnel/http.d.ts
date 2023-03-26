ade2668e67196f7a122aa8093e78b0fee4dabccb
Tony Hu <hujingyuan25@163.com>
2023-03-27 05:31:57 +0800

    feat: support http tunnel; support web-app

--- core/dist/tunnel/http.d.ts
+++
@@ -1,6 +0,0 @@
-interface ExtraInit {
-    keepResponseHeaders?: string[];
-}
-export declare function fetch(url: RequestInfo | URL, init?: RequestInit & ExtraInit): Promise<Response>;
-export declare function getCsrfToken(): string | undefined;
-export {};
--- core/dist/types/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * as theme from './theme';
-export * as webApp from './web-app';
