f92d360a4bbe7c10183d6ff559098cbfea4cafd4
Tony Hu <hujingyuan25@163.com>
2023-08-20 21:21:24 +0800

    lint: update eslintrs file

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,6 +32,7 @@ module.exports = {
         "@typescript-eslint",
         "react"
     ],
+    "ignorePatterns": ["node_modules/", "dist/", "server/", "webpack.config.js", "third-party/"],
     "rules": {
         "no-unused-vars": "off",
         "@typescript-eslint/no-unused-vars": [
